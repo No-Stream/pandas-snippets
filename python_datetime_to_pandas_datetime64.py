0ca08b7a9a0d9305f5ea9eaca1bd2034026bc491
Jan F-F <a70mik@gmail.com>
2021-03-28 13:52:56 -0700

    don't modify index

--- python_datetime_to_pandas_datetime64.py
+++ python_datetime_to_pandas_datetime64.py
@@ -5,10 +5,11 @@ def py_dt_to_pd_dt(ser: pd.Series, tz: str = "UTC") -> pd.Series:
     """
     Weirdly, we lack some tz ops on Series, so we have to coerce to index and then back to Series.
     """
+    orig_index = ser.index.copy()
     ts = (
         pd.DatetimeIndex(pd.to_datetime(ser.apply(lambda x: pd.Timestamp(x)), utc=True))
         .tz_convert(tz)
         .to_series()
     )
+    ts.index = orig_index  # reindex throwing errs
     return ts
-	
