8576eaa7dccc89bb7c7f2880759828cbff90d396
Jan F-F <a70mik@gmail.com>
2021-07-15 17:53:26 -0700

    better low cardinality numeric handling

--- plot_univariate.py
+++ plot_univariate.py
@@ -3,6 +3,7 @@ import warnings
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from pandas.api.types import is_numeric_dtype
 
 
 def plot_univariate(
@@ -15,7 +16,6 @@ def plot_univariate(
 ) -> None:
     """
     Plot a list of features compared to outcome.
-
     df: pd.DataFrame, containing relevant data
     feats: list[str], colnames of x features to graph against your outcome
     y_col: str, name of your outcome column, assume it's continuous
@@ -24,101 +24,106 @@ def plot_univariate(
     graph_sample: int, how many data points to use for scatter graphs (gets messy with too many)
     cat_freq_thresh: float, % of the non-NA values of the column must be x in order to graph it.
       i.e. ignore very rare cats.
-
     return: None, will display graphs
     """
 
     is_binary_outcome = df[y_col].nunique() == 2
 
     for feat in feats:
+        try:
+            this_df = df.copy().dropna(subset=[feat, y_col])
 
-        this_df = df.copy().dropna(subset=[feat, y_col])
-
-        is_cat = (this_df[feat].dtype == "object") or (
-            hasattr(this_df[feat], 'cat')
-        )
-
-        cardinality = this_df[feat].nunique()
-        rel_cardinality = cardinality / len(this_df)
-
-        is_dt = "datetime" in this_df[feat].dtype.name  # HACK
-
-        if (is_cat or (rel_cardinality < cat_thresh)) and not is_dt:
-            freqs = this_df[feat].value_counts(normalize=True)
-            # Filter on level of var occurred 100+ times; sort by freq
-            freqs = freqs.loc[freqs >= cat_freq_thresh]
-            levels_to_eval = freqs.index
-
-            plt.figure(figsize=(12, 8))
-            sns.catplot(
-                x=feat,
-                y=y_col,
-                data=this_df,
-                kind="point",
-                join=False,
-                order=levels_to_eval,
+            is_cat = (this_df[feat].dtype == "object") or (
+                hasattr(this_df[feat], 'cat')
             )
-            plt.xticks(rotation=90)
-            plt.title(f"Outcome {y_col} vs {feat}")
-            plt.show()
 
-        # consider dt to be integer seconds since UNIX epoch
-        else:
-            if is_dt:
-                min_ts = this_df[feat].min()
-                epoch_since_min = (
-                    pd.to_datetime(this_df[feat]) - pd.to_datetime("1970-01-01")
-                ) / pd.to_timedelta("1s")
-                # transform to set min ts at 0 for readability
-                min_epoch = epoch_since_min.min()
-                epoch_since_min = epoch_since_min - min_epoch
+            cardinality = this_df[feat].nunique()
+            rel_cardinality = cardinality / len(this_df)
 
-                empirical1pct, empirical99pct = (
-                    epoch_since_min.quantile(0.01),
-                    epoch_since_min.quantile(0.99),
-                )
-                fil_outliers = (epoch_since_min >= empirical1pct) & (
-                    epoch_since_min <= empirical99pct
-                )
+            is_dt = "datetime" in this_df[feat].dtype.name  # HACK
+            is_numeric = is_numeric_dtype(this_df[feat])
 
-                sns.regplot(
-                    x=epoch_since_min.sample(graph_sample, random_state=42),
-                    y=this_df[y_col].sample(graph_sample, random_state=42),
-                    scatter_kws={"alpha": 0.2},
-                    lowess=True,
+            if (is_cat or (rel_cardinality < cat_thresh)) and not is_dt and not is_numeric:
+                freqs = this_df[feat].value_counts(normalize=True)
+                # Filter on level of var occurred 100+ times; sort by freq
+                freqs = freqs.loc[freqs >= cat_freq_thresh]
+                levels_to_eval = freqs.index
+
+                plt.figure(figsize=(12, 8))
+                sns.catplot(
+                    x=feat,
+                    y=y_col,
+                    data=this_df,
+                    kind="point",
+                    join=False,
+                    order=levels_to_eval,
                 )
-                plt.title(f"{y_col} vs {feat} (as seconds since min.)")
+                plt.xticks(rotation=90)
+                plt.title(f"Outcome {y_col} vs {feat}")
                 plt.show()
 
-            # numeric feature, use regplot
-            else:
-                try:
-                    # confirm it can be cast to float
-                    _ = this_df[feat].astype("float")
+            # consider dt to be integer seconds since UNIX epoch
+            elif is_dt and not is_numeric:
+
+                    min_ts = this_df[feat].min()
+                    epoch_since_min = (
+                        pd.to_datetime(this_df[feat]).dt.tz_convert(tz='UTC') 
+                        - pd.to_datetime("1970-01-01").tz_localize('UTC')
+                    ) / pd.to_timedelta("1s")
+                    # transform to set min ts at 0 for readability
+                    min_epoch = epoch_since_min.min()
+                    epoch_since_min = epoch_since_min - min_epoch
 
                     empirical1pct, empirical99pct = (
-                        this_df[feat].quantile(0.01),
-                        this_df[feat].quantile(0.99),
+                        epoch_since_min.quantile(0.01),
+                        epoch_since_min.quantile(0.99),
                     )
-                    fil_outliers = (this_df[feat] >= empirical1pct) & (
-                        this_df[feat] <= empirical99pct
+                    fil_outliers = (epoch_since_min >= empirical1pct) & (
+                        epoch_since_min <= empirical99pct
                     )
-
                     graph_sample = min(graph_sample, len(this_df.loc[fil_outliers]))
-                    
-                    plot_lowess = not is_binary_outcome
+
                     sns.regplot(
-                        x=feat,
-                        y=y_col,
-                        data=this_df.loc[fil_outliers].sample(
-                            graph_sample, random_state=42
-                        ),
+                        x=epoch_since_min.sample(graph_sample, random_state=42),
+                        y=this_df[y_col].sample(graph_sample, random_state=42),
                         scatter_kws={"alpha": 0.2},
-                        lowess=plot_lowess,
-                        logistic=is_binary_outcome,
+                        lowess=True,
                     )
-                    plt.title(f"Regress {feat} on {y_col}.")
+                    plt.title(f"{y_col} vs {feat} (as seconds since min.)")
                     plt.show()
-                except Exception as err:
-                    warnings.warn(err)
-                    pass
+
+            # numeric feature, use regplot
+            elif is_numeric:
+                # confirm it can be cast to float
+                _ = this_df[feat].astype("float")
+
+                empirical1pct, empirical99pct = (
+                    this_df[feat].quantile(0.01),
+                    this_df[feat].quantile(0.99),
+                )
+                fil_outliers = (this_df[feat] >= empirical1pct) & (
+                    this_df[feat] <= empirical99pct
+                )
+
+                graph_sample = min(graph_sample, len(this_df.loc[fil_outliers]))
+
+                plot_lowess = not is_binary_outcome
+                sns.regplot(
+                    x=feat,
+                    y=y_col,
+                    data=this_df.loc[fil_outliers].sample(
+                        graph_sample, random_state=42
+                    ),
+                    scatter_kws={"alpha": 0.2},
+                    lowess=plot_lowess,
+                    logistic=is_binary_outcome,
+                )
+                plt.title(f"Regress {feat} on {y_col}.")
+                plt.show()
+                
+            else:
+              warnings.warn(f"Unhandled column {feat}")
+              
+        except Exception as err:
+            warnings.warn(err)
+            pass
